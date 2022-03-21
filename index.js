a663446af95833c757d3df76ee78cfde02f76c56
kaushik <kaushiksheel9@gmail.com>
2022-03-21 19:07:38 +0530

    '.'

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const cars = require("./routes/cars");
 
 app.use(express.json());
-app.use(cors());
+
 
 
 app.use("/", cars);
